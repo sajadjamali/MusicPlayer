b321c9381de2f8d3390ff81a73481b470b26dc64
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2023-11-22 22:04:14 +0330

    y

--- src/Component/ErrorPage.jsx
+++ src/Component/ErrorPage.jsx
@@ -1,11 +1,11 @@
-import error from "../assets/gifs/error.gif";
+// import error from "../assets/gifs/error.gif";
 import Particle from "./Particle";
 
 const ErrorPage = () => {
     return (
         <>
             <section className="h-screen flex flex-col justify-center items-center">
-                <img src={error} className="rounded-full w-6/12" alt="not found" />
+                {/* <img src={error} className="rounded-full w-6/12" alt="not found" /> */}
                 <p className="text-white font-bold text-4xl mt-4">Error....🤦‍♂️</p>
             </section>
             <Particle />
